d9c155c6c9e7052443e9654156ca68b52fd14110
AnantGabhane <gabhaneanant90@gmail.com>
2022-04-25 19:00:25 +0530

    Cheak if number is prime or not

---
+++ prime.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    bool isprime = true;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            isprime = false;
+            break;
+        }
+    }
+        if(isprime = true)
+            cout << "Number is prime number" << endl;
+        else{
+            cout << "Number is not a prime number" << endl;
+        }
+    
+    return 0;
+}
+
+/*
+Enter a number: 7
+Number is prime number
+*/
