8c59edaaaaa70ad60554f70f73a043fcbb48f03d
AnantGabhane <gabhaneanant90@gmail.com>
2022-04-25 16:45:49 +0530

    print index of 2 nested loops

---
+++ practice.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout << i << " " << j << endl;
+        }
+    }
+    return 0;
+}
+
+/*
+1 1
+1 2
+1 3
+1 4
+1 5
+2 1
+2 2
+2 3
+2 4
+2 5
+3 1
+3 2
+3 3
+3 4
+3 5
+4 1
+4 2
+4 3
+4 4
+4 5
+5 1
+5 2
+5 3
+5 4
+5 5
+*/ 
