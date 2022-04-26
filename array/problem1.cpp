da329286b7180bff1094452c940cdd5cc38de98a
AnantGabhane <gabhaneanant90@gmail.com>
2022-04-26 20:10:09 +0530

    Linear seach in an array

--- array/problem1.cpp
+++ array/problem1.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-   int search, c, n=6;
+   int search, i, n=6;
    int arr[] = {12, 35, 69, 74, 165, 54};
    search=165;
-   for (c = 0; c < n; c++) {
-      if (arr[c] == search) {
-         printf("%d is present at location %d.\n", search, c+1);
+   for (i = 0; i < n; i++) {
+      if (arr[i] == search) {
+         printf("%d is present at Index %d.\n", search, i);
          break;
       }
    }
-   if (c == n)
+   if (i == n)
       printf("%d isn't present in the array.\n", search);
    return 0;
 }
