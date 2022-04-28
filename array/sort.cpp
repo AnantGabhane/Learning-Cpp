734146deb8167d910689f09fddab6c41c62b8376
Anant Gabhane <gabhaneanant90@gmail.com>
2022-04-28 19:05:34 +0530

    Sorted array

--- array/sort.cpp
+++ array/sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int sortzerone(int arr[], int n){
     int one =0,zero=0 ,two=0;
-    for(i=0; i<n; i++){
+    for(int i = 0; i<n; i++){
         if(arr[i]==0)
             zero++;
         else if(arr[i]==1)
@@ -20,8 +20,22 @@ int sortzerone(int arr[], int n){
         arr[i] =1;
         i++;
     }
-    while(){
-        
+    while(two--){
+        arr[i] =2;
+        i++;
     }
-    
+    return 0; 
 }
+
+int main() {
+    int arr[]={1,0,1,2,2,0,1};
+    sortzerone(arr, 7);
+
+    for(int i=0; i< 7; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;   
+}
+/*
+0 0 1 1 1 2 2 
+*/
