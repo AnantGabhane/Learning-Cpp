48c1384643daafaab998a1f17a4a32dc693bbbdd
Anant Gabhane <gabhaneanant90@gmail.com>
2022-04-28 19:12:32 +0530

    Cheak array if its palindrome or not

--- array/cheakp.cpp
+++ array/cheakp.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+bool cheakpalindrome(int arr[], int n){
+    int i=0;
+    int j = n-1;
 
-int min(){
-    
+    while(i<j){
+        if(arr[i]==arr[j]){
+            i++;
+            j--;
+        }else{
+            return false;
+        }
+    }
+    return true;
 }
+
+int main(){
+    int arr[] = {1,2,3,3,2,1};
+    cout << "Palindrome or not " << cheakpalindrome(arr,6)<<endl;
+    return 0;
+}
+
+// 1 its palindrome ..... if 0 its not a palindrome
